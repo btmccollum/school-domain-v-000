391e639245802ba11e7390ffd9e58c527a41e4f9
Brad McCollum <btmcco8@gmail.com>
2018-08-17 18:00:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,8 +17,9 @@ class School
   def add_student(name, grade)
     @name = name
     @grade = grade
-    @roster["#{@grade}"] = []
     binding.pry
+    @roster["#{@grade}"] = []
+
     @roster[@grade] << "#{@name}"
   end
 # binding.pry
